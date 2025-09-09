25f716c3d32cf83bebe252efef4f77b5c73b1430
FredrikAlmen93 <Fredrik.x.almen@gmail.com>
2025-09-09 18:18:17 +0200

    Array printing method created

--- src/main/java/se/iths/fredrik/projekt/Main.java
+++ src/main/java/se/iths/fredrik/projekt/Main.java
@@ -2,6 +2,13 @@ package se.iths.fredrik.projekt;
 
 public class Main {
     public static void main(String[] args) {
+        String[] greeting = {"Hello", "world", "!"};
+        printArray(greeting);
+    }
 
+    public static void printArray(String[] hello) {
+        for (int i = 0; i < hello.length; i++) {
+            System.out.println(hello[i]);
+        }
     }
 }
