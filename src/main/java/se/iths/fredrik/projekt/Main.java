68ca4c5c639ada2793d43d35da962e6bddcdc695
FredrikAlmen93 <Fredrik.x.almen@gmail.com>
2025-09-15 12:46:23 +0200

    Max number finder-method created

--- src/main/java/se/iths/fredrik/projekt/Main.java
+++ src/main/java/se/iths/fredrik/projekt/Main.java
@@ -2,13 +2,30 @@ package se.iths.fredrik.projekt;
 
 public class Main {
     public static void main(String[] args) {
+        //Print Array iterations, one at a time
         String[] greeting = {"Hello", "world", "!"};
         printArray(greeting);
+
+        //Print max number from Array
+        int[] numbers = {1, 10, 15, 20, 3, 44, 51, 40};
+        System.out.println("The max number of the array is: " + findMax(numbers));
     }
 
+    //ARRAY PRINTER
     public static void printArray(String[] hello) {
         for (String i : hello) {
             System.out.println(i);
         }
+    }
+
+    //MAX NUMBER FINDER
+    public static int findMax(int[] maxArray) {
+        int max = maxArray[0];
+        for (int i = 0; i < maxArray.length; i++) {
+            if (maxArray[i] > max) {
+                max = maxArray[i];
+            }
+        }
+        return max;
     }
 }
